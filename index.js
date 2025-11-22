6426b55e60e59f8b219b7bd9d1bd695dfddd4ff5
munchmakeup <munchmakeup@gmail.com>
2025-11-22 17:03:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ connectDB();
 
 const app = express();
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: true })); 
 app.use(cors({
     origin: [
     'http://localhost:3000',
